71ecb8ff62843a08907f2011e7002c1d0eda1d9f
Nerun <39396637+neruneru0419@users.noreply.github.com>
2019-02-01 09:22:34 +0900

    調整した
    調整した

--- rankobot.rb
+++ rankobot.rb
@@ -1,7 +1,5 @@
 require 'twitter'
  
-#クライアントを作成
-#twClientにキー情報を格納
 twClient = Twitter::REST::Client.new do |config|
   config.consumer_key    = ENV['MY_CONSUMER_KEY']
   config.consumer_secret = ENV['MY_CONSUMER_SECRET']
@@ -9,23 +7,20 @@ twClient = Twitter::REST::Client.new do |config|
   config.access_token_secret = ENV['MY_ACCESS_TOKEN_SECRET']
 end
 
-rankopic = []
 flg = true
-
 loop do
 	time = Time.new
+	
 	if time.min % 20 == 0 && flg then
-		#ツイートする
 		passion = rand(400) + 1
 		puts passion
-		rankopic << File.new("ranko/neruneru#{passion}.png")
+		rankopic = File.new("ranko/neruneru#{passion}.png")
 		res = twClient.update_with_media("このツイートはパッションの#{passion}枚目のスクショです", rankopic)
 		puts res
-		rankopic.clear
 		flg = false
 	end
+		
 	if time.min % 20 == 1 then
 		flg = true
 	end
-
 end
