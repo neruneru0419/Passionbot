350f87b0c0769698d1d2add2098656e1f5ca8b32
Nerun <39396637+neruneru0419@users.noreply.github.com>
2019-04-24 09:50:05 +0900

    awsから画像読み込み

--- rankobot.rb
+++ rankobot.rb
@@ -1,5 +1,6 @@
 require 'twitter'
- 
+require 'aws-sdk-v1'
+
 twClient = Twitter::REST::Client.new do |config|
   config.consumer_key    = ENV['MY_CONSUMER_KEY']
   config.consumer_secret = ENV['MY_CONSUMER_SECRET']
@@ -7,14 +8,21 @@ twClient = Twitter::REST::Client.new do |config|
   config.access_token_secret = ENV['MY_ACCESS_TOKEN_SECRET']
 end
 
+AWS.config({
+    :access_key_id => ENV['AWS_ACCESS_KEY'],
+    :secret_access_key => ENV['AWS_ACCESS_SECRET'],
+})
+s3 = AWS::S3.new
+bucket = s3.buckets["neruneru"]
 flg = true
 loop do
 	time = Time.new
 	
 	if time.min % 20 == 0 && flg then
-		passion = rand(400) + 1
+        passion = rand(1081) + 1
+        pic = bucket.objects["ranko/ranko#{passion}.png"]
 		puts passion
-		rankopic = File.new("ranko/neruneru#{passion}.png")
+		rankopic = pic.read
 		res = twClient.update_with_media("このツイートはパッションの#{passion}枚目のスクショです", rankopic)
 		puts res
 		flg = false
